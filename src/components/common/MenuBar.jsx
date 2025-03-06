59cba2a25e8b684d4cfe9f8a912fc0f3a43f0e95
devkansara <kansaradev874@gmail.com>
2025-03-06 16:39:07 +0530

    menubar component

--- src/components/common/MenuBar.jsx
+++ src/components/common/MenuBar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Hamburger from 'hamburger-react'
+import DarkModeToggle from './DarkModeToggle'
 function MenuBar() {
+  const [isOpen,setOpen]=useState(false)
   return (
     <div>
-      this is menubar
+      <ul className='bg-lightmode w-60 p-2 sm:w-72 sm:p-4 rounded-md
+      dark:bg-darkmode-components
+      '>
+        <li className='hover:dark:bg-red-500 hover:rounded-md hover:bg-red-500 p-3'>kdk</li>
+        <li className='hover:dark:bg-red-500 hover:rounded-md hover:bg-red-500 p-3'>jhj</li>
+        <li className='hover:dark:bg-red-500 hover:rounded-md hover:bg-red-500 p-3'>lhu</li>
+        <li className='hover:dark:bg-red-500 hover:rounded-md hover:bg-red-500 p-3'>iu</li>
+        <li className='hover:dark:bg-red-500 hover:rounded-md hover:bg-red-500 p-3'>oi</li>
+        <DarkModeToggle/>
+      </ul>
     </div>
   )
 }
