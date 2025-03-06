582d98a76f6247c7fb108437a705c0bfbf984d45
devkansara <kansaradev874@gmail.com>
2025-03-06 16:45:11 +0530

    added darkmode and lighmode theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default {content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        // 'darkmode': '#121212da',
+        'darkmode-bg':"#343434",
+        'darkmode-components':"#424242",
+        'lightmode':'#f3f4f6'
+        // You can customize this color
+      },
+      textColor: {
+        'darkmode': '#ffffff',
+        'lightmode':'#f3f4f6'
+        // You can customize this color
+      },
+    },
   },
   plugins: [],
 }
-
