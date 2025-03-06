3ef6e1edb4e6fb3012a197bda55306bd39708858
devkansara <kansaradev874@gmail.com>
2025-03-06 16:43:11 +0530

    loading navbar to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from './components/common/NavBar'
+import MenuBar from './components/common/MenuBar'
+import DarkModeToggle from './components/common/DarkModeToggle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <div className="main">
+      <NavBar />
+    </div>
     </>
   )
 }
