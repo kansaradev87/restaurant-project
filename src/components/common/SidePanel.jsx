53408bf5903306c205ad0842c7d9f9dc163119b7
devkansara <kansaradev874@gmail.com>
2025-03-07 15:32:04 +0530

    sidebar components for quick access to links

---
+++ src/components/common/SidePanel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SidePanel() {
+  return (
+    <div className="dark:border-0 flex ">
+      <div className='h-52 bg-lightmode  dark:bg-darkmode-components md:rounded-2xl rounded-none 
+        w-full dark:border-0 dark:text-darkmode shadow-2xl
+        
+
+      '>
+        sidepanel
+      </div>
+    </div>
+  )
+}
+
+export default SidePanel
