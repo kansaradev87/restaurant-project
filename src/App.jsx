0df2d56ce78ab054005f7cc65d8be37f9e910035
devkansara <kansaradev874@gmail.com>
2025-03-07 15:27:25 +0530

    added hero and sidepanel components with responsive tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import NavBar from './components/common/NavBar'
 import MenuBar from './components/common/MenuBar'
 import DarkModeToggle from './components/common/DarkModeToggle'
+import SidePanel from './components/common/SidePanel'
+import Hero from './components/common/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +13,14 @@ function App() {
     <>
     <div className="main">
       <NavBar />
+      <div className='flex justify-center pt-4 dark:bg-darkmode-bg'>
+        <aside className='w-1/6 ' >
+          <SidePanel />
+        </aside>
+        <aside className='pl-4 w-4/6'>
+          <Hero />
+        </aside>
+      </div>
     </div>
     </>
   )
