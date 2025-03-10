68f4dd780800ba278ca62f15a5d18d5848cde46a
devkansara <kansaradev874@gmail.com>
2025-03-10 15:53:16 +0530

    contains all employee components

---
+++ src/components/employee/EmployeeMainComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import DarkModeToggle from '../common/DarkModeToggle'
+
+function EmployeeMainComponent() {
+  return (
+    <div className="dark:border-0 flex ">
+      <div className='md:h-[82vh] h-screen bg-lightmode  dark:bg-darkmode-components md:rounded-2xl rounded-none 
+        w-full dark:border-0 dark:text-darkmode shadow-2xl
+        md:w-full sm:w-full
+      '>
+        EmployeeMainComponent
+        <DarkModeToggle />
+      </div>
+    </div>
+  )
+}
+
+export default EmployeeMainComponent
