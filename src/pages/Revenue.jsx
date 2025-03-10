fa58ab7c9ef7209d17ee48feda53864bb03720f1
devkansara <kansaradev874@gmail.com>
2025-03-10 15:51:58 +0530

    created pages folder containing necessary pages

---
+++ src/pages/Revenue.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import NavBar from '../components/common/NavBar'
+import SidePanel from '../components/common/SidePanel'
+import Hero from '../components/common/Hero'
+import RevenueMainComponent from '../components/revenue/RevenueMainComponent'
+function Revenue() {
+  return (
+    <div>
+      <div className="main">
+           <NavBar />
+            <div className='flex justify-center pt-0  dark:bg-darkmode-bg sm:pt-0 md:pt-4'>
+                <aside className=' lg:w-1/6 lg:visible ' >
+                    <SidePanel />
+                </aside>
+                <aside className=' pl-0 lg:pl-4 w-screen lg:w-4/6 md:w-5/6 md:justify-center sm:w-screen sm:justify-center sm:pl-0 sm:pt-0'>
+                    <RevenueMainComponent />
+                    
+                </aside>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Revenue
