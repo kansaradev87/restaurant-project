0a6c36e7c84e8282021615b70d99fb52f70f6caa
devkansara <kansaradev874@gmail.com>
2025-03-11 15:56:31 +0530

    all these components now use outlet so all of them are loaded accordingly

--- src/components/expense/ExpenseMainComponent.jsx
+++ src/components/expense/ExpenseMainComponent.jsx
@@ -4,12 +4,11 @@ import DarkModeToggle from '../common/DarkModeToggle'
 function ExpenseMainComponent() {
   return (
     <div className="dark:border-0 flex ">
-      <div className='md:h-[82vh] h-screen bg-lightmode  dark:bg-darkmode-components md:rounded-2xl rounded-none 
+      <div className='md:h-[82.5vh] h-screen bg-lightmode  dark:bg-darkmode-components md:rounded-2xl rounded-none 
         w-full dark:border-0 dark:text-darkmode shadow-2xl
         md:w-full sm:w-full
       '>
         ExpenseMainComponent
-        <DarkModeToggle />
       </div>
     </div>
   )
