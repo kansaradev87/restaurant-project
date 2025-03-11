450a22f48d7a0cb0683e103086bc1ac096cb5d61
devkansara <kansaradev874@gmail.com>
2025-03-11 16:17:24 +0530

    added margin bottom on medium screen sizes

--- src/components/revenue/RevenueMainComponent.jsx
+++ src/components/revenue/RevenueMainComponent.jsx
@@ -6,7 +6,7 @@ function RevenueMainComponent() {
     <div className="dark:border-0 flex ">
       <div className='md:h-[82.5vh] h-screen bg-lightmode  dark:bg-darkmode-components md:rounded-2xl rounded-none 
         w-full dark:border-0 dark:text-darkmode shadow-2xl
-        md:w-full sm:w-full
+        md:w-full sm:w-full md:mb-5
       '>
         RevenueMainComponent
       </div>
