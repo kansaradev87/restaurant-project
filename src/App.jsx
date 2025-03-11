801ebdc8ce6f2ace7fc7c513c5606f8678c60787
devkansara <kansaradev874@gmail.com>
2025-03-11 15:50:26 +0530

    changed the routing added layout as the main route and used outlet

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,20 @@ import Table from './pages/Table'
 import Expense from './pages/Expense'
 import Revenue from './pages/Revenue'
 import Employee from './pages/Employee'
+import Layout from './pages/Layout'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/table' element={<Table />}  />
-          <Route path='/expense' element={<Expense />}  />
-          <Route path='/revenue' element={<Revenue />}  />
-          <Route path='/employee' element={<Employee />}  />
-
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='table' element={<Table />} />
+            <Route path='expense' element={<Expense />} />
+            <Route path='revenue' element={<Revenue />} />
+            <Route path='employee' element={<Employee />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
