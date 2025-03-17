dcbfc57f95ae21cd3b5719c1a46dec3838c65b15
devkansara <kansaradev874@gmail.com>
2025-03-17 16:48:52 +0530

    added darkmode toggle

--- src/components/common/MenuBar.jsx
+++ src/components/common/MenuBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import DarkModeToggle from './DarkModeToggle'
 function MenuBar({onLinkClick}) {
   const [isOpen,setOpen]=useState(false)
   return (
@@ -9,7 +10,7 @@ function MenuBar({onLinkClick}) {
       dark:bg-darkmode-components
       '>
         <NavLink to='/' onClick={onLinkClick} className={({isActive}) => 
-              `block rounded-md p-3 text-lg font-bol duration-200 
+              `block rounded-md p-3 text-lg  duration-200 
               ${isActive ? 
                 'bg-lightmode-active text-lightmode-active-text dark:bg-darkmode-active dark:text-darkmode-active-text' : 
                 'hover:bg-lightmode-hover hover:text-lightmode-hover-text dark:hover:bg-darkmode-hover dark:hover:text-darkmode-hover-text'
@@ -18,7 +19,7 @@ function MenuBar({onLinkClick}) {
               <li >Home</li>
         </NavLink>
         <NavLink to='/table' onClick={onLinkClick} className={({isActive}) => 
-              `block rounded-md p-3 text-lg font-bol duration-200 
+              `block rounded-md p-3 text-lg  duration-200 
               ${isActive ? 
                 'bg-lightmode-active text-lightmode-active-text dark:bg-darkmode-active dark:text-darkmode-active-text' : 
                 'hover:bg-lightmode-hover hover:text-lightmode-hover-text dark:hover:bg-darkmode-hover dark:hover:text-darkmode-hover-text'
@@ -27,7 +28,7 @@ function MenuBar({onLinkClick}) {
               <li>Table</li>
         </NavLink>
         <NavLink to='/expense' onClick={onLinkClick} className={({isActive}) => 
-              `block rounded-md p-3 text-lg font-bol duration-200 
+              `block rounded-md p-3 text-lg  duration-200 
               ${isActive ? 
                 'bg-lightmode-active text-lightmode-active-text dark:bg-darkmode-active dark:text-darkmode-active-text' : 
                 'hover:bg-lightmode-hover hover:text-lightmode-hover-text dark:hover:bg-darkmode-hover dark:hover:text-darkmode-hover-text'
@@ -36,7 +37,7 @@ function MenuBar({onLinkClick}) {
               <li>Expense</li>
         </NavLink>
         <NavLink to='/revenue' onClick={onLinkClick} className={({isActive}) => 
-              `block rounded-md p-3 text-lg font-bol duration-200 
+              `block rounded-md p-3 text-lg  duration-200 
               ${isActive ? 
                 'bg-lightmode-active text-lightmode-active-text dark:bg-darkmode-active dark:text-darkmode-active-text' : 
                 'hover:bg-lightmode-hover hover:text-lightmode-hover-text dark:hover:bg-darkmode-hover dark:hover:text-darkmode-hover-text'
@@ -45,7 +46,7 @@ function MenuBar({onLinkClick}) {
               <li>Revenue</li>
         </NavLink>
         <NavLink to='/employee' onClick={onLinkClick} className={({isActive}) => 
-              `block rounded-md p-3 text-lg font-bol duration-200 
+              `block rounded-md p-3 text-lg  duration-200 
               ${isActive ? 
                 'bg-lightmode-active text-lightmode-active-text dark:bg-darkmode-active dark:text-darkmode-active-text' : 
                 'hover:bg-lightmode-hover hover:text-lightmode-hover-text dark:hover:bg-darkmode-hover dark:hover:text-darkmode-hover-text'
@@ -53,6 +54,10 @@ function MenuBar({onLinkClick}) {
             }>
               <li>Employee</li>
           </NavLink>
+          <div className='flex  rounded-md p-3 text-lg  duration-200'>
+            Dark Mode
+            <DarkModeToggle />
+          </div>
       </ul>
     </div>
   )
