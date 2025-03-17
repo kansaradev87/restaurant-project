1aed205b644af4e461833c21497a1d17a1f7fea7
devkansara <kansaradev874@gmail.com>
2025-03-17 16:08:14 +0530

    updated code to close the menubar when a link is clicked

--- src/components/common/MenuButton.jsx
+++ src/components/common/MenuButton.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import Hamburger from 'hamburger-react'
-import DarkModeToggle from './DarkModeToggle'
 import MenuBar from './MenuBar'
     function MenuButton() {
     const [isOpen,setOpen]=useState(false)
+    const closeMenu=()=>{
+        setOpen(false)
+    }
     return (
         <div>
             <div className="relative flex items-center">
@@ -11,10 +13,10 @@ import MenuBar from './MenuBar'
                 <div className="text-md">
                     <Hamburger toggled={isOpen} color="#b8b8b8" toggle={setOpen} />
                 </div>
-                {/* Dropdown Menu (Opens Below) */}
+                {/* Dropdown Menu (Opens Below) added onclick close  */}
                 {isOpen && (
                     <div className="absolute top-full left-0 mt-2 bg-lightmode dark:bg-darkmode shadow-lg rounded-lg">
-                        <MenuBar />
+                        <MenuBar onLinkClick={closeMenu} />
                     </div>
                 )}
             </div>
