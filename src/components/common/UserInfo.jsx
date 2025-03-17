df0a61f33dc0b42064d2b310690083b90bc144e2
devkansara <kansaradev874@gmail.com>
2025-03-17 16:13:14 +0530

    remover unnecessary information

--- src/components/common/UserInfo.jsx
+++ src/components/common/UserInfo.jsx
@@ -6,18 +6,11 @@ function UserInfo() {
     <div className='border p-2 mt-3 rounded-2xl h- bg-lightmode  dark:bg-darkmode-components md:rounded-2xl  
         w-full dark:border-0 dark:text-darkmode shadow-2xl
         hidden lg:block'>
-      userinfo
       <div className='flex items-center'>
-        <div className="name">
-            username
-        </div>
         <br />
-        <div className="photo ">
-            <img src="https://placehold.co/50x50" alt="" className='rounded-full float-left ml-28'/>
-        </div>
       </div>
       <div className="name mt-0">
-            restaurant mail
+            username
         </div>
         <div className="appearance flex items-center ">
             <div className=''>
