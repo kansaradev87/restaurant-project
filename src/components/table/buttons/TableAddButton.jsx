6f88c8e1a308a5799ffd57c990b2a54fd79303de
devkansara <kansaradev874@gmail.com>
2025-03-24 16:43:22 +0530

    managed states

--- src/components/table/buttons/TableAddButton.jsx
+++ src/components/table/buttons/TableAddButton.jsx
@@ -1,44 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { IoMdAdd } from "react-icons/io";
-import AddTableForm from '../forms/AddTableForm';
 
-function TableAddButton() {
-    const [display, setDisplay] = useState(false);  // Control the visibility of AddTableForm
-
-    // Function to toggle the display state when the button is clicked
+function TableAddButton({ isStandalone = true, onClick }) {
+  // Only use internal state when component is used standalone
+  // Otherwise, rely on the parent component to control visibility
+  
+  if (isStandalone) {
+    // Original implementation for standalone use
+    const [display, setDisplay] = useState(false);
+    
     const addTable = () => {
-        setDisplay(true);  // Show the AddTableForm
+      setDisplay(true);
     }
 
     return (
-        <div>
-        {/* Conditionally render the button only when display is false */}
-                {!display && (
-                    <div className='
-                    flex justify-center items-center
-                    md:justify-start md:items-start'>
-                    <button
-                    className='
-                    border border-black dark:border-darkmode
-                    w-full h-48 mr-2 ml-2 mb-2 mt-2
-                    rounded-lg  
-                    md:w-full md:h-64
-                    md:ml-2 md:mr-2
-                    flex justify-center items-center 
-                    hover:bg-lightmode-hover dark:hover:bg-darkmode-hover
-                    transition-all duration-300 ease-in-out
-                    ' 
-                    onClick={addTable}
-                >
-                    <IoMdAdd className='text-2xl' />
-                    Add Table {/* Add icon styling */}
-                </button>
-                </div>
-                )}
-        {/* Conditionally render the AddTableForm when display is true */}
-            {display && <AddTableForm setDisplay={setDisplay} />}
-        </div>
+      <div>
+        {!display && (
+          <div className='flex justify-center items-center md:justify-start md:items-start'>
+            <button
+              className='border border-black dark:border-darkmode w-full h-48 mr-2 ml-2 mb-2 mt-2 rounded-lg md:w-full md:h-64 md:ml-2 md:mr-2 flex justify-center items-center hover:bg-lightmode-hover dark:hover:bg-darkmode-hover transition-all duration-300 ease-in-out'
+              onClick={addTable}
+            >
+              <IoMdAdd className='text-2xl' />
+              Add Table
+            </button>
+          </div>
+        )}
+        {display && <AddTableForm setDisplay={setDisplay} />}
+      </div>
     );
+  } else {
+    // Simplified version to be controlled by parent
+    return (
+      <div className='flex justify-center items-center md:justify-start md:items-start'>
+        <button
+          className='border border-black dark:border-darkmode w-full h-48 mr-2 ml-2 mb-2 mt-2 rounded-lg md:w-full md:h-64 md:ml-2 md:mr-2 flex justify-center items-center hover:bg-lightmode-hover dark:hover:bg-darkmode-hover transition-all duration-300 ease-in-out'
+          onClick={onClick}
+        >
+          <IoMdAdd className='text-2xl' />
+          Add Table
+        </button>
+      </div>
+    );
+  }
 }
 
 export default TableAddButton;
