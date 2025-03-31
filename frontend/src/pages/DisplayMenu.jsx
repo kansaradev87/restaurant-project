b7ca7741dcdb9ff79d6336f1687d37d3a18acd10
devkansara <kansaradev874@gmail.com>
2025-04-01 00:38:29 +0530

    new page display menu added

---
+++ frontend/src/pages/DisplayMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DisplayMenuComponent from '../components/menu/DisplayMenuComponent'
+
+function DisplayMenu() {
+  return (
+      <div>
+          <aside className=' pl-0 lg:pl-4 w-screen md:w-full md:justify-center sm:w-screen sm:justify-center sm:pl-0 sm:pt-0'>
+              <DisplayMenuComponent />
+              
+          </aside>
+      </div>
+    
+  )
+}
+
+export default DisplayMenu;
