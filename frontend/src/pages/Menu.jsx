739fcafde791a561f1e1836ac9b2f225ed5276c7
devkansara <kansaradev874@gmail.com>
2025-04-01 00:38:41 +0530

    no change

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -3,7 +3,6 @@ import MenuMainComponent from '../components/menu/MenuMainComponent'
 function Menu() {
   return (
     <div>
-
                 <aside className=' pl-0 lg:pl-4 w-screen md:w-full md:justify-center sm:w-screen sm:justify-center sm:pl-0 sm:pt-0'>
                     <MenuMainComponent />
                     
