8f9d630f1f6dbd7325589a035abf7b5b2d78474a
devkansara <kansaradev874@gmail.com>
2025-04-01 16:13:02 +0530

    added all necessary routes

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -4,7 +4,11 @@ const categoryController= require('../controllers/categoryController');
 
 //routes
 
-router.get("/",categoryController.createCategory);
+router.post("/",categoryController.createCategory);
+router.get("/",categoryController.getAllCategories);
+router.get('/:name',categoryController.getOneCategory);
+router.delete('/:name',categoryController.deleteCategory);
+router.put("/",categoryController.updateCategory);
 
 
 module.exports=router;
