020227537a3c0a09254aff54d3f41586ee4c467d
devkansara <kansaradev874@gmail.com>
2025-04-01 16:53:35 +0530

    created buttons to crud items

---
+++ frontend/src/components/menu/item/buttons/AddItemButton.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { IoMdAdd } from "react-icons/io";
+import AddItemForm from '../forms/AddItemForm';
+
+function AddItemButton({ isStandalone = true, onClick }) {
+  // Only use internal state when component is used standalone
+  // Otherwise, rely on the parent component to control visibility
+  
+  if (isStandalone) {
+    // Original implementation for standalone use
+    const [display, setDisplay] = useState(false);
+    
+    const addTable = () => {
+      setDisplay(true);
+    }
+
+    return (
+      <div>
+        {!display && (
+          <div className='flex justify-center items-center md:justify-start md:items-start'>
+            <button
+              className='border border-black dark:border-darkmode w-full h-48 mr-2 ml-2 mb-2 mt-2 rounded-lg md:w-full md:h-64 md:ml-2 md:mr-2 flex justify-center items-center hover:bg-lightmode-hover dark:hover:bg-darkmode-hover transition-all duration-300 ease-in-out'
+              onClick={addTable}
+            >
+              <IoMdAdd className='text-2xl' />
+              Add Category
+            </button>
+          </div>
+        )}
+        {display && <AddItemForm setDisplay={setDisplay} />}
+      </div>
+    );
+  } else {
+    // Simplified version to be controlled by parent
+    return (
+      <div className='flex justify-center items-center md:justify-start md:items-start'>
+        <button
+          className='border border-black dark:border-darkmode w-full h-48 mr-2 ml-2 mb-2 mt-2 rounded-lg md:w-full md:h-64 md:ml-2 md:mr-2 flex justify-center items-center hover:bg-lightmode-hover dark:hover:bg-darkmode-hover transition-all duration-300 ease-in-out'
+          onClick={onClick}
+        >
+          <IoMdAdd className='text-2xl' />
+          Add Item
+        </button>
+      </div>
+    );
+  }
+}
+
+export default AddItemButton;
---
+++ frontend/src/components/menu/item/buttons/DisplayItemButton.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { IoMdEye } from "react-icons/io";
+import DisplayItem from '../forms/DisplayItem';
+
+function DisplatItemButton() {
+  const [display, setDisplay] = useState(false);
+
+  const showCategories = () => {
+    setDisplay(true);  // Show the list of tables
+  };
+
+  return (
+    <div>
+      {!display && (
+        <div className='flex justify-center items-center md:justify-start md:items-start'>
+          <button
+            className='border border-black dark:border-darkmode w-full h-48 mr-2 ml-2 mb-2 mt-2 rounded-lg md:w-full md:h-64 md:ml-2 md:mr-2 flex justify-center items-center hover:bg-lightmode-hover dark:hover:bg-darkmode-hover transition-all duration-300 ease-in-out'
+            onClick={showCategories}
+          >
+            <IoMdEye className='text-2xl' />
+            Display Items
+          </button>
+        </div>
+      )}
+      {display && <DisplayItem setDisplay={setDisplay} />}
+    </div>
+  );
+}
+
+export default DisplatItemButton;
